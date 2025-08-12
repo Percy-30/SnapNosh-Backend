8f55f30dd8ab9a2f41138841e5a5c0f2878b034d
Percy-30 <achataipepercy2000@gmail.com>
2025-08-12 17:11:11 -0500

    coie update

--- app/routes/cookies_routes.py
+++ app/routes/cookies_routes.py
@@ -1,17 +1,31 @@
 import os
 from fastapi import APIRouter, BackgroundTasks, HTTPException, Request
 from app.services.youtube_cookie_updater import login_youtube_and_save_cookies
-
+import boto3
 
 router = APIRouter()
 API_TOKEN = os.getenv("CRON_SECRET_TOKEN")
+
+def upload_to_s3(filename, bucket, key):
+    s3 = boto3.client('s3')
+    s3.upload_file(filename, bucket, key)
+    print(f"✅ Archivo subido a s3://{bucket}/{key}")
 
 def update_youtube_cookies_task():
     EMAIL = os.getenv("YOUTUBE_EMAIL")
     PASSWORD = os.getenv("YOUTUBE_PASSWORD")
     OUTPUT_PATH = os.getenv("YOUTUBE_COOKIES_PATH", "cookies.txt")
+    S3_BUCKET = os.getenv("S3_BUCKET_NAME")
+    S3_KEY = os.getenv("S3_COOKIES_KEY", "cookies.txt")
+
     if EMAIL and PASSWORD:
+        print(f"Ejecutando login para {EMAIL}, guardando en {OUTPUT_PATH}")
         login_youtube_and_save_cookies(EMAIL, PASSWORD, OUTPUT_PATH)
+        print("Cookies actualizadas correctamente.")
+        
+        # Subir cookies a S3 si configurado
+        if S3_BUCKET:
+            upload_to_s3(OUTPUT_PATH, S3_BUCKET, S3_KEY)
     else:
         print("❌ No se puede actualizar cookies, faltan variables de entorno")
 
